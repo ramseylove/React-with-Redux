9d37f8aff5ef01f424e495d18b35622e2aa269ac
Ryan Valentine <ryan@atriadev.com>
2021-11-06 06:42:39 -0600

    reformatted by prettier

--- local-api-client/src/actions/index.js
+++ local-api-client/src/actions/index.js
@@ -1,19 +1,20 @@
-import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from "../apis/streams";
+import { SIGN_IN, SIGN_OUT } from "./types";
 
-export const signIn = userId => {
+export const signIn = (userId) => {
   return {
     type: SIGN_IN,
-    payload: userId
+    payload: userId,
   };
 };
 
 export const signOut = () => {
   return {
-    type: SIGN_OUT
+    type: SIGN_OUT,
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+// Action creator
+export const createStream = (formValues) => async (dispatch) => {
+  streams.post("/streams", formValues);
 };
